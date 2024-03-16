25dc4c44f9c25be3812a65403a0bd24966aab363
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-03-15 21:10:46 -0500

    Refactory

--- modules/fxtr/src/main/java/org/coderic/iso20022/externalcodes/fxtr/AdditionalInformation5.java
+++ modules/fxtr/src/main/java/org/coderic/iso20022/externalcodes/fxtr/AdditionalInformation5.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.03.15 a las 07:25:30 PM COT 
+// Generado el: 2024.03.15 a las 08:18:21 PM COT 
 //
 
 
