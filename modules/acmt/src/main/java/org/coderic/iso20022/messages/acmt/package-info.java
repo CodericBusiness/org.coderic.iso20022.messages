c639e15a3528991714115aa905c5e458f64349db
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-01-16 15:56:13 -0500

    Fix Paths

--- modules/acmt/src/main/java/org/coderic/iso20022/messages/acmt/package-info.java
+++ modules/acmt/src/main/java/org/coderic/iso20022/messages/acmt/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.2 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2025.01.11 a las 06:39:09 AM COT 
+// Generado el: 2025.01.16 a las 03:52:14 PM COT 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:acmt.037.001.02", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
--- modules/fxtr/src/main/java/org/coderic/iso20022/messages/fxtr/MessageIdentification1.java
+++ modules/fxtr/src/main/java/org/coderic/iso20022/messages/fxtr/MessageIdentification1.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.2 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2025.01.11 a las 06:39:30 AM COT 
+// Generado el: 2025.01.16 a las 03:53:53 PM COT 
 //
 
 
